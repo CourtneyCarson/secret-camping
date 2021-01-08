9b995d5782ef86d662e60db53d75e4782cac36b8
Courtney Carson <cmacarson@gmail.com>
2021-01-08 14:00:41 -0800

    saving location to user account works!

--- src/04.SiteList/SiteList.js
+++ src/04.SiteList/SiteList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import AddNewSite from '../07.AddSiteForm/AddSiteForm';
 import TokenService from '../services/token-service';
 import config from '../config';
 import './SiteList.css';
@@ -11,9 +10,31 @@ export default class SiteList extends Component {
     super(props);
     this.state = {
       error: null,
+      locationId: "",
       location: [],
     };
   }
+
+  // handle submit for saving to users account button:
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { locationId } = event.target;
+    console.log('locationID', locationId);
+
+    this.setState({ error: null });
+    
+    let currentUserId = TokenService.getUserId();
+    console.log('currentUserId', currentUserId);
+    this.postLocation(locationId.value);
+    console.log('locationId.value', locationId.value)
+    // this.postLocation(location.id.value);
+
+    // this.props.history.push('/account');
+  };
+
+
+
+
 
   // fetch req for all locations
   componentDidMount() {
@@ -38,9 +59,41 @@ export default class SiteList extends Component {
 
   // potentially add the star rating to the cards also 
 
+  //  POST - SAVE LOCATION TO USER ACCOUNT 
+  postLocation(location_id) {
+    // let id = this.props.match.params.id;
+    let URL = `${config.API_ENDPOINT}/userloc/${location_id}`;
+    // let URL = `${config.API_ENDPOINT}/userloc`;
+
+
+
+    return fetch(URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        location_id,
+      }),
+    })
+      // .then((res) => res.json());
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => this.setState({ error: e })
+          )
+          : res.json()
+    )
+    .catch(err => {
+      // console.log(err)
+      this.setState({ error: err });
+  })
+  }
+
 
   render() {
     console.log(this.state.location);
+
     return (
 
 
@@ -59,25 +112,30 @@ export default class SiteList extends Component {
             return (
               <div className='site-list' key={key}>
 
-                <p>{item.title}</p>
-                <p>{item.content}</p>
-                <img src={item.image} alt='location' />
-                <p>{item.keyword}</p>
-                <p>{item.location}</p>
-                <button className='save-button' type='submit'> Save </button>
+                <form className='locations-div' onSubmit={this.handleSubmit}>
+                  <p>{item.title}</p>
+                  <p>{item.content}</p>
+                  <img src={item.image} alt='location' />
+                  <p>{item.keyword}</p>
+                  <p>{item.location}</p>
+                  
+                  <input type='hidden' name='locationId' value={item.id}></input>
+                  <button className='save-button' type='submit'> Save </button>
 
-                {/* google map */}
-                <iframe
-                  className="item-image"
-                  width="50%"
-                  height="250"
-                  id="google_map"
-                  src={iFrameUrl}
-                  frameBorder="0"
-                  scrolling="no"
-                  alt={item.keyword}
-                  title='title'
-                ></iframe>
+                  {/* google map */}
+                  <iframe
+                    className="item-image"
+                    width="50%"
+                    height="250"
+                    id="google_map"
+                    src={iFrameUrl}
+                    frameBorder="0"
+                    scrolling="no"
+                    alt={item.keyword}
+                    title='title'
+                  ></iframe>
+
+                </form>
 
               </div>
             );
