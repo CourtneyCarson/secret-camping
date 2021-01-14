2003218a1be71f8fbb9cd4b1fac99cf318c14446
Courtney Carson <cmacarson@gmail.com>
2021-01-13 18:04:16 -0800

    landing css

--- src/01.Landing/Landing.js
+++ src/01.Landing/Landing.js
@@ -9,9 +9,8 @@ export default class Landing extends Component {
     return (
 
       <main>
-        <div className='landing-page'>
-          <header>words</header>
-
+        {/* <div className='landing-page'> */}
+          
           <section className='landing-overlay'>
           <div className='landing-page-header'>
             <h1 className='landing-h1'> Secret Camping Spots </h1>
@@ -19,12 +18,12 @@ export default class Landing extends Component {
 
           <div className='landing-page-content'>
             <p> A members only site to share the best secret camping spots. No campsites. No other people. Just you and nature.</p>
-              <Link to='/login'><button>Log In</button> </Link>
-              <Link to='/signup'><button>Register</button> </Link>
+              <Link to='/login'><button className='landing-btn'>Log In</button> </Link>
+              <Link to='/signup'><button className='landing-btn'>Register</button> </Link>
 
           </div>
           </section>
-        </div>
+        {/* </div> */}
       </main>
     );
   }
