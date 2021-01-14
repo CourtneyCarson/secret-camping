567fe61ec868ec48d85902309df2ccd41be410ed
Courtney Carson <cmacarson@gmail.com>
2021-01-13 17:54:59 -0800

    css style for sign-up page

--- src/03.SignUp/SignUp.js
+++ src/03.SignUp/SignUp.js
@@ -68,43 +68,45 @@ class SignUp extends Component {
 
     return (
       <section className='sign-up-component'>
-        <div className="sign-up-page">
-          <h1 className='sign-up-title'>Secret Camping </h1>
-          <div className="form-div-reg">
 
-            <h3 className="header">Sign Up</h3>
-            <form className="signup-form" onSubmit={this.handleSubmit}>
-              {errorMessage}
+        <section className='sign-up-overlay'>
+          <div className="sign-up-page">
+            <h1 className='sign-up-title'>Secret Camping </h1>
+            <div className="form-div-reg">
 
-              <label className="signup-label">Email
+              <h3 className="header">Sign Up</h3>
+              <form className="signup-form" onSubmit={this.handleSubmit}>
+                {errorMessage}
+
+                <label className="signup-label">Email
               <input
-                  className="sign-up-input"
-                  type="text"
-                  name="registerUsername"
-                  placeholder="email@email.com"
-                  required />
-              </label>
+                    className="sign-up-input"
+                    type="text"
+                    name="registerUsername"
+                    placeholder="email@email.com"
+                    required />
+                </label>
 
-              <label className="signup-label">Password
+                <label className="signup-label">Password
               <input
-                  className="sign-up-input"
-                  type="password"
-                  name="registerPassword"
-                  placeholder="password"
-                  required />
-              </label>
+                    className="sign-up-input"
+                    type="password"
+                    name="registerPassword"
+                    placeholder="password"
+                    required />
+                </label>
 
-              <button type="submit" className="sign-up-button">Register</button>
+                <button type="submit" className="sign-up-button">Register</button>
 
-            </form>
-            <div className="link-register-div">
-              <p> Already Have An Account?
+              </form>
+              <div className="link-register-div">
+                <p> Already Have An Account?
               <Link to="/login" className="login-link">Log In</Link></p>
-            </div>
+              </div>
 
+            </div>
           </div>
-        </div>
-      </section>
+        </section></section>
     );
   }
 }
