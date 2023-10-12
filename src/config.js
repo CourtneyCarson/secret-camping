51cf00cc754118cc58d9f7320f645ac0eb4075a3
Courtney Carson <67847371+CourtneyCarson@users.noreply.github.com>
2023-10-11 17:55:07 -0700

    Update config.js
    update to render endpoint instead of heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_ENDPOINT: 'https://secret-camping.herokuapp.com/api',
+  // API_ENDPOINT: 'https://secret-camping.herokuapp.com/api',
+  API_ENDPOINT: 'https://secret-camping-server.onrender.com',
   // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'secret-camp-capstone-auth-token',
 };
